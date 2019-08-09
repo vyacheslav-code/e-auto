efaa90948e697740a06fb77fed7e955967e99ef8
vyacheslav-code <vyacheslav.code@gmail.com>
2019-08-10 00:55:28 +0300

    * server cache

--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', '/build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+    res.writeHead(200, {
+        'Cache-Control': 'no-cache'
+    })
 });
 
 const port = process.env.PORT || 5000;
